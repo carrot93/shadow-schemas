5e4403beb39495a39663cd98205f3412a0602af2
Kestanous <CMTopus@me.com>
2015-03-14 17:12:06 -0400

    ready for publish

--- package.js
+++ package.js
@@ -8,7 +8,7 @@ Package.on_use(function (api, where) {
   api.use([
     'matb33:collection-hooks@0.7.5',
     'mreaction:shadow-collections@0.2.0', 
-    'underscore', 
+    'underscore@1.0.0', 
     'mreaction:shadow-util@0.1.0'
   ]);
   api.add_files('lib/schema.js');
